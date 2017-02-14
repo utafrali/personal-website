70a00833f5fb6f66f1f5fb7a9770fda82e9b4bfd
Jack <me@jack003.com>
2017-02-14 15:03:09 +0800

    edit some place

--- build/dev-server.js
+++ build/dev-server.js
@@ -5,17 +5,17 @@ var fileConf = require('./files.conf.js')
 var CSSJSfiles = fileConf.CSSJSfiles;
 
 exec('npm install', function(code, stdout, stderr) {
-  if (code == 0) {
-  	console.log("Packages are already installed.\n".green)
-  } else {
-  	console.log('Exit code:', code);
-	  console.log('Program output:', stdout);
-	  console.log('Program stderr:', stderr);
-  }
+    if (code == 0) {
+        console.log("Packages are already installed.\n".green)
+    } else {
+        console.log('Exit code:', code);
+        console.log('Program output:', stdout);
+        console.log('Program stderr:', stderr);
+    }
 });
 
 exec('jekyll server --watch', function(code, stdout, stderr) {
-  console.log('Exit code:', code);
-  console.log('Program output:', stdout);
-  console.log('Program stderr:', stderr);
+    console.log('Exit code:', code);
+    console.log('Program output:', stdout);
+    console.log('Program stderr:', stderr);
 });
